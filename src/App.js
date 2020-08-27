6b137c43dc9191dce14c87292fd6d86c2ff35f40
ROBERT ANGEL TORRES <rtorres61595@gmail.com>
2020-08-27 11:21:31 -0400

    react profile

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{width: '100%', margin: 'auto'}}>
+        <div className="landing-grid">
+            
+
+            <div className="banner-text">
+              <h1>Robert Torres</h1>
+              <p>Full Stack Web Developer with a ​background in sales and customer service and relations as well security.
+
+              Recognized for innovation and a keen eye for aesthetics and presentation. ​Excellent interpersonal communication skills and collaborator who works well on team-based projects as well as independently.
+
+            Recognized for proven complex problem solving abilities to contribute effectively as a part of a fast-paced, quality-driven team.
+            
+
+            </p>
+            
+            <hr/>
+
+          <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
+
+        <div className="social-links">
+
+          <button>
+          <a href="https://www.linkedin.com/in/robert-torres-495921141/" rel="noopener noreferrer" target="_blank">
+          </a>Linkedin</button>
+
+          <button>
+          <a href="https://github.com/rtorres61595" rel="noopener noreferrer" target="_blank">
+          </a>
+          github</button>
+        </div>
+            </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
